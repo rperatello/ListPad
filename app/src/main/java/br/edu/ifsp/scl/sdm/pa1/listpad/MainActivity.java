e3f8add10dfa91f5b99778a6596514b7a6e58ae3
Roger Peratello <roger.peratello@hotmail.com>
2021-11-27 12:32:51 -0300

    Início do projeto.

---
+++ app/src/main/java/br/edu/ifsp/scl/sdm/pa1/listpad/MainActivity.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.scl.sdm.pa1.listpad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
